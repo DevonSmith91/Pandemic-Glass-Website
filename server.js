88a7939b88d336e64ab20c1b85068b5f5ac39699
Devon <devonsmith91@gmail.com>
2020-10-21 11:13:47 -0400

    contact page mostly finished and functioning

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+// pre nodemailer requirements
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
 const cloudinary = require("cloudinary");
+
+
 
 // defining the path for the server to launch the html page form
 app.use(express.static(path.resolve("./client/build")));
@@ -59,9 +62,72 @@ app.get("/collabImages", async (req, res) => {
 //   );
 // });
 
+//post nodemailer requirements
+const router = express.Router();
+const nodemailer = require("nodemailer");
+const creds = require("./config");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+
+const transport = {
+  host: "smtp.gmail.com",
+  port: 587,
+  auth: {
+    user: creds.USER,
+    pass: creds.PASS,
+  },
+};
+
+let transporter = nodemailer.createTransport(transport);
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("server is ready to take messages");
+  }
+});
+
+app.post("/send", (req, res, next) => {
+  console.log("this is the req", req.body)
+  let name = req.body.name;
+  let email = req.body.email;
+  let message = req.body.message;
+  let content = `Name: ${name}\nEmail: ${email}\nMessage:${message}`;
+
+  let mail = {
+    from: name,
+    to: "devon@pandemic.glass",
+    subject: "New Message From Contact Form",
+    text: content,
+  };
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: "fail",
+      });
+    } else {
+      res.json({
+        status: "success",
+      });
+    }
+  });
+});
+
+app.use(cors())
+app.use(express.json())
+
 // telling the page to display home when given an address
 app.get("*", (req, res) => {
   res.sendFile(path.resolve("./client/build/index.html"));
 });
 // giving a port that the server is being hosted on
 app.listen(port, () => console.log(`Listening on port: ${port}`));
+
+
+
